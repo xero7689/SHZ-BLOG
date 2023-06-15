73a5272c2c465ef44465204820dd1967d9e24db0
xero7689 <volleyp7689@gmail.com>
2023-06-15 15:05:58 +0800

    Update: Use .env file to control project secret key

--- shzleeBlog/settings.py
+++ shzleeBlog/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3(&%$4%=f7v!jf--uc&7bo=utfjntp(=pi)2xi-q5b_y40x%s5'
+# SECRET_KEY = 'django-insecure-3(&%$4%=f7v!jf--uc&7bo=utfjntp(=pi)2xi-q5b_y40x%s5'
+SECRET_KEY = environment.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEPLOY_STAGE = environment.DEPLOY_STAGE
