474e4eb71775acb08fd7801d8eea4080ebad1566
xero7689 <volleyp7689@gmail.com>
2023-07-13 16:22:10 +0800

    feature: Add collapse button to hljs block

--- blog/static/blog/js/index.js
+++ blog/static/blog/js/index.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   const appBarElement = document.getElementById("appbar-wrapper");
   const appBarHeight = appBarElement.offsetHeight;
 
+  // AppBar Display Handler
   let scrollHandler = (function () {
     let previousScrollTop = $(this).scrollTop();
     let isHiding = false;
@@ -24,4 +25,45 @@ $(document).ready(function () {
   })();
 
   $(window).scroll(scrollHandler);
+
+  /*
+   * Code Snippets Collapse Button
+   */
+  // Collapse Button Factory
+  function createCollapseButton(preBlock) {
+    let button = document.createElement("div");
+    let icon = document.createElement("i");
+
+    //
+    icon.classList.add("fa-solid");
+    icon.classList.add("fa-chevron-down");
+
+    // button.textContent = "Expand..";
+    button.classList.add("hljs-collapse-btn");
+    button.appendChild(icon);
+
+    button.onclick = () => {
+      preBlock.classList.toggle("active");
+      icon.classList.toggle("fa-chevron-down");
+      icon.classList.toggle("fa-chevron-up");
+    };
+
+    return button;
+  }
+
+  const preBlocks = document.getElementsByTagName("pre");
+  for (let i = 0; i < preBlocks.length; i++) {
+    let preBlock = preBlocks[i];
+
+    if (preBlock.querySelectorAll(".hljs")) {
+      let preRect = preBlock.getBoundingClientRect();
+      if (preRect.height < 256) {
+        preBlock.classList.toggle("active");
+        preBlock.classList.add("pre-not-overflow");
+      } else {
+        let collapseButton = createCollapseButton(preBlock);
+        preBlock.after(collapseButton);
+      }
+    }
+  }
 });
